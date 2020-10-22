14c9dce182b45c3e87a423b680ad862f7a0bc790
Mustafa CEYLAN <mustafa19.mc@gmail.com>
2020-10-22 13:29:14 +0300

    Presentition Kuruldu

---
+++ Cakmak.Yapi.Presentation/Areas/Admin/Controllers/HomeMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cakmak.Yapi.Presentation.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class HomeMController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
