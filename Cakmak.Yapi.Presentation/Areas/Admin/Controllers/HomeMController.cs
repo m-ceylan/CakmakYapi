54e7e723d2ee34fb612412567edf012fe361201a
Mustafa CEYLAN <mustafa19.mc@gmail.com>
2021-01-10 14:26:32 +0300

    qwe

--- Cakmak.Yapi.Presentation/Areas/Admin/Controllers/HomeMController.cs
+++ Cakmak.Yapi.Presentation/Areas/Admin/Controllers/HomeMController.cs
@@ -13,5 +13,11 @@ namespace Cakmak.Yapi.Presentation.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Test()
+        {
+
+            return View();
+        }
     }
 }
